4249b45ef019cdc6e718b6597c38407e10115cb4
Lucia416 <lucia.awang416@gmail.com>
2017-02-13 23:55:24 +0000

    exercise done

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
-var request = require('request');
-
+var request = require("request");
+var GITHUB_USER = "Lucia416";
+var GITHUB_TOKEN = "ce9a634450fd399dcfae5f7d39ad18b106d5466c";
 console.log('Welcome to the GitHub Avatar Downloader');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+var option = {url : requestURL, headers: {'User-Agent': "GitHub Avatar Downloader - Student Project" } };
+
+  request(option, function (err, result, body){
+    cb(err,JSON.parse(body))
+  console.log(body);
+});
+}
+
+getRepoContributors("jquery", "jquery", function(err,body) {
+  var avatarArray = [];
+  body.forEach(function(name){
+    avatarArray.push(body.avatar_url)
+  })
+  console.log("Errors:", err);
+  console.log("Result:", avatarArray);
+});
