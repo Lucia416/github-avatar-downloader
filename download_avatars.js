8c7b6ee22ef001ddfbc1a243316c0dba0c2f0979
Lucia416 <lucia.awang416@gmail.com>
2017-02-14 01:06:08 +0000

    path 7 and I have also corrected the whole file as I have realized I did some mistake in my previous submission

--- download_avatars.js
+++ download_avatars.js
@@ -1,24 +1,30 @@
 var request = require("request");
+var fs = require('fs');
 var GITHUB_USER = "Lucia416";
 var GITHUB_TOKEN = "ce9a634450fd399dcfae5f7d39ad18b106d5466c";
 console.log('Welcome to the GitHub Avatar Downloader');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-var option = {url : requestURL, headers: {'User-Agent': "GitHub Avatar Downloader - Student Project" } };
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-  request(option, function (err, result, body){
-    cb(err,JSON.parse(body))
-  console.log(body);
-});
+  var option = {url : requestURL, headers: {'User-Agent': "GitHub Avatar Downloader - Student Project" } };
+  //
+  request(option, cb);
 }
 
-getRepoContributors("jquery", "jquery", function(err,body) {
-  var avatarArray = [];
-  body.forEach(function(name){
-    avatarArray.push(body.avatar_url)
+
+getRepoContributors("jquery", "jquery", function(err, result, body) {
+  var parse = JSON.parse(body);
+  parse.forEach(function(name){
+    downloadImageByURL(name.avatar_url, name.login);
   })
+  function downloadImageByURL(url, filePath) {
+    request.get(url)
+    .pipe(fs.createWriteStream(`./Avatar/${filePath}.jpg`));
+    // ...
+  }
+/*
   console.log("Errors:", err);
-  console.log("Result:", avatarArray);
+  console.log("Result:", body); */
 });
